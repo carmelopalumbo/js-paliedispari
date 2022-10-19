f9b255477769b72b55fe2c8f18dd0799f19c4c03
carmelopalumbo <carmelopalumbo@outlook.it>
2022-10-19 16:24:02 +0200

    add-cpuchoice

--- PARI&DISPARI/js/script.js
+++ PARI&DISPARI/js/script.js
@@ -6,17 +6,32 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto. */
 
-const userChoice = document.querySelector('.scelta_utente').value;
+const userChoice = document.querySelector('.scelta_utente');
 
-const userNum = document.querySelector('.num_utente').value;
+const userNum = document.querySelector('.num_utente');
 
-const inputBtn = document.querySelector('.btn');
+const inputBtn = document.querySelector('.gioca');
+const resetBtn = document.querySelector('.reset');
+let cpuChoice;
 
-console.log(userChoice);
-console.log(userNum);
+console.log(userChoice.value);
+console.log(userNum.value);
 
+resetBtn.addEventListener('click', function(){
+    userChoice.selectedIndex = 0;
+    userNum.selectedIndex = 0;
+})
 
+inputBtn.addEventListener('click',function(){
+    if(userChoice.value == 'pari'){
+        cpuChoice = 'dispari';
+    }else{
+        cpuChoice = 'pari';
+    }
+    console.log(cpuChoice);
+})
 
+// funzioni
 function randomNumber(){
     return Math.floor(Math.random() * 5) + 1;
 }
