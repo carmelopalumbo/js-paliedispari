f7f0b5095b1bcafc93bf8166590c7319f28e3409
carmelopalumbo <carmelopalumbo@outlook.it>
2022-10-19 17:45:50 +0200

    fixed-es2

--- PARI&DISPARI/js/script.js
+++ PARI&DISPARI/js/script.js
@@ -6,50 +6,54 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto. */
 
+//variabili
 const userChoice = document.querySelector('.scelta_utente');
 const inputBtn = document.querySelector('.gioca');
 const resetBtn = document.querySelector('.reset');
-
+const winResult = document.querySelector('.result');
+const outputCpu = document.querySelector('.num_cpu');
 let userNum = document.querySelector('.num_utente');
 
-// console.log(userChoice.value);
-// console.log(userNum.value);
 
+//resetta tutti campi
 resetBtn.addEventListener('click', function(){
     userChoice.selectedIndex = 0;
     userNum.selectedIndex = 0;
+    winResult.innerHTML = '';
+    outputCpu.innerHTML = '';
 })
 
 inputBtn.addEventListener('click',function(){
 
-    let cpuNum = randomNumber();
-
-    if(userChoice.value == 'pari'){
-       console.log(cpuNum);
-       console.log(checkPariDispari());
-       if(checkPariDispari(userNum, cpuNum) === 'pari'){
-        console.log('GIOCATORE VINCE');
-       }else{
-        console.log('CPU VINCE');
-       }
+    //blocca il programma se non vengono inseriti entrambi i valori
+    if(userNum.value === "" || userChoice === "" ){
+        winResult.innerHTML = 'INSERISCI VALORI VALIDI!';
     }else{
-        console.log(cpuNum);
-        if(checkPariDispari(userNum, cpuNum) === 'dispari'){
-         console.log('GIOCATORE VINCE');
+        //verifico il vincitore
+        let cpuNum = randomNumber();
+        outputCpu.innerHTML = `Il numero scelto dalla CPU é ${cpuNum}.`
+        if(userChoice.value === checkPariDispari(userNum.value, cpuNum)){
+            console.log('utente vince');
+            winResult.innerHTML = 'UTENTE VINCE!'
         }else{
-         console.log('CPU VINCE');
+            console.log('cpu vince');
+            winResult.innerHTML = 'CPU VINCE!'
+            }
         }
-    }
-})
+    })
 
 // funzioni
+
+//estraggo un numero random da 1 a 5
 function randomNumber(){
     return Math.floor(Math.random() * 5) + 1;
 }
 
+//fa la somma dei due numeri e verifica se pari o dispari
 function checkPariDispari(num1, num2){
-    const somma = num1 + num2;
-
+    // console.log(num1, num2);
+    const somma = parseInt(num1) + parseInt(num2);
+    console.log(somma);
     if(somma % 2) return 'dispari' ;
 
     return 'pari';
