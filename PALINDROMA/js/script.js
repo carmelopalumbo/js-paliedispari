5613ce002532411a342292fd9071af587304b88b
carmelopalumbo <carmelopalumbo@outlook.it>
2022-10-19 14:14:38 +0200

    add-boostrap-structure

--- PALINDROMA/js/script.js
+++ PALINDROMA/js/script.js
@@ -1,1 +1,12 @@
 // js
+
+/*Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma*/
+
+
+const firstWord = document.getElementById('parola_uno').value;
+
+const secondWord = document.getElementById('parola_due').value;
+
+console.log(firstWord);
+console.log(secondWord);
