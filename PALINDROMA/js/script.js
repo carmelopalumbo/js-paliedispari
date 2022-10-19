32172e73101220ad3eb4a4fa8f663b7e0fb029be
carmelopalumbo <carmelopalumbo@outlook.it>
2022-10-19 14:56:06 +0200

    add-es1

--- PALINDROMA/js/script.js
+++ PALINDROMA/js/script.js
@@ -4,9 +4,37 @@
 Creare una funzione per capire se la parola inserita è palindroma*/
 
 
-const firstWord = document.getElementById('parola_uno').value;
+const inputBtn = document.querySelector('.btn');
 
-const secondWord = document.getElementById('parola_due').value;
+const result = document.getElementById('risultato');
 
-console.log(firstWord);
-console.log(secondWord);
+// console.log(firstWord);
+// console.log(secondWord);
+
+inputBtn.addEventListener('click', function(){
+    let firstWord = document.getElementById('parola_uno').value.toLowerCase();
+    const check = checkPalindrome(firstWord);
+    if(check){
+        result.classList.remove('red');
+        result.innerText = 'LA PAROLA E\' PALINDROMA!';
+        result.classList.add('green');
+    }else{
+        result.classList.remove('green');
+        result.innerText = 'LA PAROLA NON E\' PALINDROMA!';
+        result.classList.add('red');
+    }
+})
+
+function checkPalindrome(p1){
+
+    let revFirst = '';
+
+    for(let i = p1.length - 1; i >= 0; i--){
+        revFirst += p1[i];
+    }
+
+
+    if(revFirst === p1) return true;
+
+    return false;
+}
